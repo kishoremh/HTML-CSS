b547ec84e6c2ae8ff08edb54c2d2281513ea76a2
kishoremh <141405643+kishoremh@users.noreply.github.com>
2024-08-31 15:28:08 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+function appendCharacter(character) {
+    document.getElementById('display').value += character;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
